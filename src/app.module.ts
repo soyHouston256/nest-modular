3f854e40de2aa7002921118f21e6039eec42bc13
Max Houston Ramirez Martel <8385004+soyHouston256@users.noreply.github.com>
2024-09-08 02:25:59 -0500

    add: proder useValue

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,17 @@ import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { CustomersModule } from './customers/customers.module';
 
+const API_KEY = 'hash-here';
+
 @Module({
   imports: [ProductsModule, UsersModule, CustomersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: API_KEY,
+    },
+  ],
 })
 export class AppModule {}
