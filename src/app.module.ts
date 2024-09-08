f7f395ea5ca834392f6ece62c4b6a8770e89569d
Max Houston Ramirez Martel <8385004+soyHouston256@users.noreply.github.com>
2024-09-08 02:34:29 -0500

    add: ConfigService to get env values

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,24 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { CustomersModule } from './customers/customers.module';
-
-const API_KEY = 'hash-here';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [ProductsModule, UsersModule, CustomersModule],
+  imports: [
+    ProductsModule,
+    UsersModule,
+    CustomersModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
     {
       provide: 'API_KEY',
-      useValue: API_KEY,
+      useFactory: (configService: ConfigService) => {
+        return configService.get<string>('API_KEY');
+      },
+      inject: [ConfigService],
     },
   ],
 })
